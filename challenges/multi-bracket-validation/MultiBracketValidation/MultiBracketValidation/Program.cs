a483644f01bd6c264c91a3bc6a3017df1ab38ae1
Benjamin Taylor <staylor.ben@gmail.com>
2019-04-03 15:30:57 -0700

    added comments

--- challenges/multi-bracket-validation/MultiBracketValidation/MultiBracketValidation/Program.cs
+++ challenges/multi-bracket-validation/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -6,22 +6,28 @@ namespace MultiBracketValidation
     public class Program
     {
         /// <summary>
-        /// 
+        /// Checks if a string contains any unclosed brackets.
         /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
+        /// <param name="input">The string being verified.</param>
+        /// <returns>A boolean representing if the string was valid.</returns>
         public static bool ValidateBrackets(string input)
         {
             char[] chars = input.ToCharArray();
             Stack<char> brackets = new Stack<char>();
             char bracket;
             
+            // Iterate through each character in the input string
             for (int i = 0; i < chars.Length; i++)
             {
+                // If the character is a opening bracket, push it to the brackets stack
                 if (chars[i] == '{' || chars[i] == '(' || chars[i] == '[')
                 {
                     brackets.Push(chars[i]);
                 }
+
+                /* If the character is a closing bracket, check if any opening brackets have been detected previously,
+                and if the previous opening bracket matches the style of the closing bracket. If either of those
+                conditions are false, false is returned. */
                 else if (chars[i] == '}' || chars[i] == ')' || chars[i] == ']')
                 {
                     try
@@ -40,6 +46,7 @@ namespace MultiBracketValidation
                 }
             }
 
+            // Returns true if all open brackets have been closed.
             try
             {
                 brackets.Pop();
@@ -55,7 +62,9 @@ namespace MultiBracketValidation
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter a string to check: ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"The string is{(ValidateBrackets(input) ? "" : " not")} valid.");
         }
     }
 }
